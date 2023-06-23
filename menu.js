e161ad4e9f81aba0618de34b0fc6dc483756cf5a
Muliro-Reis <129551298+Muliro-Reis@users.noreply.github.com>
2023-06-22 21:24:41 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,11 @@
+window.onscroll = function() {func_navbar()};
 var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function func_navbar() {
+  if(windowPageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
